01ad058e1a42e654a4a5e7fc8acdf9a6e738f36e
techmaster <techmaster@techmasters-iMac-6.local>
2017-06-07 11:35:40 +0700

    first commit

--- UISliderUISwitch/ViewController.swift
+++ UISliderUISwitch/ViewController.swift
@@ -7,19 +7,40 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var btn_Play: UIButton!
+        var audio = AVAudioPlayer()
+        var playing = true
+    @IBOutlet weak var sld_Volumee: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        audio = try! AVAudioPlayer(contentsOf: URL(fileURLWithPath: Bundle.main.path(forResource: "music", ofType: "mp3")!))
+        audio.prepareToPlay()
+        addThumImgForSlider()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+//set up
+    func addThumImgForSlider () {
+        sld_Volumee.setThumbImage(UIImage(named: "thumb.png"), for: .normal)
+        sld_Volumee.setThumbImage(UIImage(named: "thumbHightLight.png"), for: .highlighted)
     }
-
+//
+    @IBAction func action_Play(_ sender: Any) {
+        if playing {
+        audio.play()
+        btn_Play.setImage(UIImage(named: "pause.png"), for: .normal)
+        }else {
+            audio.pause()
+            btn_Play.setImage(UIImage(named: "play.png"), for: .normal)
+            
+        }; playing = !playing
+    }
+    
+    @IBAction func btn_Volume(_ sender: Any) {
+        audio.volume = (sender as AnyObject).value
+        
+    }
 
 }
 
